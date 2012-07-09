69c11012147f36f2e52058814353bfa05057e48b
Patrick <synapse.and.syntax@gmail.com>
2012-07-09 04:28:46 -0400

    major optimizations in ESA2

--- ESA.py
+++ ESA.py
@@ -1,6 +1,6 @@
 """Extended Suffix Array Class"""
 from utils import *
-
+import time
 def lcp_functional(x, y):
     """Return the length of the longest common prefix of x and y.
     Because Python does not implement TCO, this will occasionally blow
@@ -16,14 +16,38 @@ def lcp(x, y):
     #Because Python does not implement TCO, a functional
     #implementation will occasionally blow the stack (!).  That's why
     #this looks so ridiculous
-    i = 0
     length = min(len(x), len(y))
     for i, (x, y) in enumerate(zip(x, y)):
         if x != y:
             return i
     return length
 
+def lcp2(x, y, word):
+    """Return the length of the longest common prefix of x and y"""
+    #Because Python does not implement TCO, a functional
+    #implementation will occasionally blow the stack (!).  That's why
+    #this looks so ridiculous
+    i = 0
+    n = len(word) - max(x,y)
+    while i < n:
+        if word[x + i] == word[y + i]:
+#            print word[x + i],word[y + i],i
+            i += 1
+        else:
+            break
+    return i
 
+def gen_head(xs):
+    """Return first element of xs if such an element, otherwise return
+    empty list"""
+    #We do this in order to avoid computing the entire list
+    #comprehension in the assignment of ESA.skp.  This leads to a 3x
+    #speedup at the expense of slightly uglier code.
+    try:
+        return xs.next()
+    except StopIteration:
+        return []
+        
 class ESA(object):
     def __init__(self, word):
         self.word = word
@@ -31,6 +55,30 @@ class ESA(object):
         self.suf = [len(word) - len(self.suffixes[i]) + 1
                     for i in range(len(word)+1)]
         self.lcp = [0] + [lcp(x, y) for (x, y) in pairs(self.suffixes)]
-        self.skp = [min([j for j in range(i+1, len(word) + 1)
-                         if self.lcp[j] < self.lcp[i]] + [len(word) + 1])
+        print len(self.lcp)
+        self.skp = [min(gen_head((j for j in range(i+1, len(word) + 1)
+                         if self.lcp[j] < self.lcp[i])),len(word) + 1)
                          for i in range(len(word) + 1)]
+        
+        
+class ESA2(object):
+    def __init__(self, word):
+        self.word = word
+        # self.suffixes = suffixes_of(word)
+        # self.suf = [len(word) - len(self.suffixes[i]) + 1
+        #             for i in range(len(word)+1)]
+        n = len(word)
+        print "sufs"
+        self.suf = fast_sufs(word)
+        print "lcp"
+        self.lcp = [0] + [lcp2(self.suf[i], self.suf[j], word)
+                          for (i, j) in gen_pairs_range(n + 1)]
+        print len(self.lcp)
+        print "skp"
+        self.skp = [min(gen_head((j for j in xrange(i+1, n + 1)
+                         if self.lcp[j] < self.lcp[i])),n + 1)
+                         for i in xrange(n + 1)]
+        
+        
+
+print ("loaded ESA")
