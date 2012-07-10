b2e48407fd3f2c9ed87ead8961ff6be798611d4f
Patrick <synapse.and.syntax@gmail.com>
2012-07-10 09:57:02 -0400

    exploring linear-time suffix tree construction options

--- ESA.py
+++ ESA.py
@@ -1,3 +1,7 @@
+;; This buffer is for notes you don't want to save, and for Lisp evaluation.
+;; If you want to create a file, visit that file with C-x C-f,
+;; then enter the text in that file's own buffer.
+
 """Extended Suffix Array Class"""
 from utils import *
 import time
@@ -37,17 +41,6 @@ def lcp2(x, y, word):
             break
     return i
 
-def gen_head(xs):
-    """Return first element of xs if such an element, otherwise return
-    empty list"""
-    #We do this in order to avoid computing the entire list
-    #comprehension in the assignment of ESA.skp.  This leads to a 3x
-    #speedup at the expense of slightly uglier code.
-    try:
-        return xs.next()
-    except StopIteration:
-        return []
-        
 class ESA(object):
     def __init__(self, word):
         self.word = word
@@ -80,5 +73,88 @@ class ESA2(object):
                          for i in xrange(n + 1)]
         
         
+def alphabet(word):
+    return ['$'] + sorted(list(set(word)))
 
+S = 0
+L = 1
+
+def is_S(x):
+    return x is None or x == S
+
+def suffix_array(word,sigma = None):
+    #1 First bucket the suffixes into array A, fill SL_array
+    T = word + "$"
+    N = len(T)
+    sigma = alphabet(word)
+    buckets = {c:[] for c in sigma}
+    SL_array = [None for i in range(N)]
+    S_yet = False
+    dist = 0
+    i = 0
+    j = 0
+    while i < N - 1:
+        print "i:",i
+        buckets[T[i]].append(i)
+        if not T[i] == T[i + 1]:
+            print "iffing"
+            SL_array[i] = S if T[i] < T[i + 1] else L
+            # print "before:",dist, S_yet
+            # print "after:",dist, S_yet
+        else:
+            print "elsing"
+            j = i + 1
+            while T[j] == T[j + 1]:
+                j += 1
+                print "j:",j
+            val = S if T[j] > T[i] else L
+            print i,j
+#            for k in range(i,j):
+            while i < j:
+                SL_array[i] = val
+                i += 1
+            i = j - 1        
+        i += 1
+    buckets['$'] = [N - 1]
+    bucket = ([buckets[c] for c in sigma],[])#] if not c == '$'],[])
+    dists = s_dist(SL_array)
+    m = max(dists)
+    #construct s-distance lists
+    s_distance_lists = {j:{s:[b for b in buckets[s] if dists[b] == j]
+                           for s in sigma} for j in range(1,m+1)}
+    print "s distance list:",s_distance_lists
+    #sort all type-S substrings
+    print "buckets:",buckets
+    type_s_lists = {c:[i for i in buckets[c] if is_S(SL_array[i])] for c in sigma}
+    print "type_s_lists:",type_s_lists
+    for j in range(1,m + 1):
+        for c in sigma:
+            for s_d in s_distance_lists[j][c]:
+                k = s_d - j
+                type_s_list = type_s_lists[c]
+                if k in type_s_list:
+                    type_s_list.remove(k)
+                    type_s_list.insert(0,k)
+    print "type_s_lists:",type_s_lists
+                    
+                    
+    return SL_array,dists,s_distance_lists
+
+def skew(word):
+    n = len(word)
+    word += "$" * ((3 - n) % 3)
+    
+    print word
+def s_dist(xs):
+    s_yet = False
+    s_dists = []
+    dist = 0
+    for x in xs:
+        dist += 1 * s_yet
+        s_dists.append(dist)
+        if is_S(x):
+            s_yet = True
+            dist = 0
+    return s_dists
+    
 print ("loaded ESA")
